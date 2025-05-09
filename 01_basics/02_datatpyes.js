5171f0dae914f9bb54ba522f8536a157ce18b897
Noushin-ibrahim <noushinibrahim1@gmail.com>
2025-05-09 05:32:41 +0000

    datatypesummary

---
+++ 01_basics/02_datatpyes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as new version
+
+//alert (3+3) // we are using node.js, not browser
+
+console.log(3+3) // CODE readability should be high
+
+console.log("noshi")
+// bigint
+// number= 2 to 53
+//string = ""
+// boolean = true/false
+//null = standalone value, empety
+// undefined = value asign na hona
+//symbol = unique
+
+//object
+console.log typeof null
+console.log  "noshi"
+
+console.log(typeof "noshi");
+ let name ="noshi"
+ let age = 18
+ let isloggedin = false 
---
+++ 01_basics/datatype_summry.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7types : string, number, boolearn, null, undefined, symbol, bigint 
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//refrence (non primitive)
+
+// array, objects, functions
+
+const menchracter = ["captain", "zunni", "babygirl"];
+
+{
+    let myobj=
+
+    name: "Noshi",
+    age : 21,
+}
+
+const myfunction = function(){
+
+console.log("hello world");
+}
+ console.log(typeof myfunction);
+ 
