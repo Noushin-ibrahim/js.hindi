73d935cc484ad78ee185e80bf263b4fc58259196
Noushin-ibrahim <noushinibrahim1@gmail.com>
2025-05-20 09:43:45 +0000

    num 06_num_and_math

---
+++ 01_basics/05_sting.js
@@ -0,0 +1,27 @@
+const name= "noshi_zubair"
+const repoCount = 40
+
+//console.log(name+repoCount+"zubair");//
+
+//console.log(`hello my name is${name}and my repo is ${repoCount}`);
+
+//console.log(name.charAt(2));
+//console.log(name.indexOf("s"));
+
+const newString = name.substring(0,4)
+//console.log(newString);
+
+const anotherstring = name.slice(-2,1)
+//console.log(anotherstring);//
+
+const newStringOne = " noshi  "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https:hitesh.com/hitesh%20khan"
+console.log(url.replace('%20',"-"));
+
+console.log(url.includes("sunadr"));
+
+console.log();
+
---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,31 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString("en-IN"));
+
+//********MATHS */
+
+//console.log(Math.abs(-4));
+//.log(Math.round(4,6));
+//console.log(Math.ceil(4,2));
+//console.log(Math.floor(4,9));
+//console.log(Math.min(4,5,7,2,));
+//console.log(Math.max(4,4,6,7,8));
+
+
+
+
+
+
+
+
+
+
+
