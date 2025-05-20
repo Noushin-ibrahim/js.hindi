7456602d36c97d537a2fab3f84f0b72849fa45d3
Noushin-ibrahim <noushinibrahim1@gmail.com>
2025-05-20 09:57:13 +0000

    Updated num and math file

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -13,13 +13,18 @@ const hundreds = 100000
 //********MATHS */
 
 //console.log(Math.abs(-4));
-//.log(Math.round(4,6));
-//console.log(Math.ceil(4,2));
-//console.log(Math.floor(4,9));
+//.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
 //console.log(Math.min(4,5,7,2,));
 //console.log(Math.max(4,4,6,7,8));
 
+console.log((Math.random()*10)+1);
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
 
 
 
